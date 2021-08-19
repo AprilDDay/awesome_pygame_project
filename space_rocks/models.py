dfa9e899a2accd2582ce1b18a5264eb4b2afa310
April Day <april.day@gmail.com>
2021-08-19 15:25:39 +0100

    add sound to laser shooting bullets

--- space_rocks/models.py
+++ space_rocks/models.py
@@ -1,7 +1,7 @@
 from pygame.math import Vector2
 from pygame.transform import rotozoom
 
-from utils import load_sprite, wrap_position, get_random_velocity
+from utils import load_sprite, wrap_position, get_random_velocity, load_sound
 
 UP = Vector2(0, -1)
 
@@ -33,6 +33,7 @@ class Spaceship(GameObject):
 
     def __init__(self, position, create_bullet_callback):
         self.create_bullet_callback = create_bullet_callback
+        self.laser_sound = load_sound("laser")
         self.direction = Vector2(UP)
         super().__init__(position, load_sprite("spaceship"), Vector2(0))
     
@@ -48,6 +49,7 @@ class Spaceship(GameObject):
         bullet_velocity = self.direction * self.BULLET_SPEED + self.velocity
         bullet = Bullet(self.position, bullet_velocity)
         self.create_bullet_callback(bullet)
+        self.laser_sound.play()
 
 class Asteroid(GameObject):
     def __init__(self, position, create_asteroid_callback, size=3):
